9465aa453acaff1215accb4b64d3589e7ffb4a34
MugileeshwaranS <mugileeshwarans@bgca.org>
2025-02-12 18:19:30 +0530

    Added a key in the .env file, need to update

--- src/api/contact/controllers/contact.ts
+++ src/api/contact/controllers/contact.ts
@@ -8,9 +8,9 @@ import CryptoJS from 'crypto-js';
 export default factories.createCoreController('api::contact.contact', ({ strapi }) => ({
     async create(ctx) {
       try {
-        const secretKey = '0244387ac5f95d2f5ae4b5e560e4c617f4b59857378d6579041229fdbb44dee9'; // Same key used in Angular
+        const secretKey = '0244387ac5f95d2f5ae4b5e560e4c617f4b59857378d6579041229fdbb44dee9';
         const encryptedData = ctx.request.body.data;
-  
+      
         // Decrypt the data
         const bytes = CryptoJS.AES.decrypt(encryptedData, secretKey);
         const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));

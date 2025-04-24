6d823ee3a936f724e984704b31fb2ec666501c28
Sankar <sankar@ilink-systesm.com>
2025-04-24 14:32:35 +0530

    dockerr integration

--- config/database.ts
+++ config/database.ts
@@ -1,4 +1,6 @@
 import path from 'path';
+const fs = require('fs');
+const rootPath = path.resolve(__dirname, '..');
 
 export default ({ env }) => {
   const client = env('DATABASE_CLIENT', 'sqlite');
@@ -11,13 +13,16 @@ export default ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        // ssl: env.bool('DATABASE_SSL', false) && {
+        //   key: env('DATABASE_SSL_KEY', undefined),
+        //   cert: env('DATABASE_SSL_CERT', undefined),
+        //   ca: env('DATABASE_SSL_CA', 'serverCa'),
+        //   capath: env('DATABASE_SSL_CAPATH', undefined),
+        //   cipher: env('DATABASE_SSL_CIPHER', undefined),
+        //   rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        ssl: {
+          //ca: fs.readFileSync(path.join(rootPath, 'certs','DigiCertGlobalRootG2.crt.pem')),
+          rejectUnauthorized: false,
         },
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
